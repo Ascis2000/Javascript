d6560c6f7a49a2ab4c9b2b17cf1dbd8194eb8932
Ascis <169037965+Ascis2000@users.noreply.github.com>
2024-07-15 10:50:55 +0200

    Add files via upload

---
+++ Peliculas/version_01/js/peliculas.js
@@ -0,0 +1,14 @@
+
+// Array general de peliculas
+let peliculas = [
+    { titulo: 'El Padrino', data: { director: 'Francis Ford Coppola', año: 1972 } },
+    { titulo: 'La Lista de Schindler', data: { director: 'Steven Spielberg', año: 1993 } },
+    { titulo: 'Pulp Fiction', data: { director: 'Quentin Tarantino', año: 1994 } },
+    { titulo: 'El Caballero Oscuro', data: { director: 'Christopher Nolan', año: 2008 } },
+    { titulo: 'Forrest Gump', data: { director: 'Robert Zemeckis', año: 1994 } }
+];
+
+let directores = [
+    { nombre: 'Steven Spielberg', films: [{ titulo: 'Tiburón' }, { titulo: 'La Lista de Schindler' }] },
+    { nombre: 'Quentin Tarantino', films: [{ director: 'Pulp Fiction' }, { titulo: 'Malditos Bastardos' }] }
+];
